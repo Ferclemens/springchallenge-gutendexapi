a9fa486c528fb4496d7637a3964e2ac278ccc2d0
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-10-20 09:47:17 -0300

    inicio de proyecto y creacion de servicios

---
+++ src/main/java/com/aluraback/desafiostreams/DesafiostreamsApplication.java
@@ -0,0 +1,13 @@
+package com.aluraback.desafiostreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiostreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiostreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluraback/desafiostreams/DesafiostreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraback.desafiostreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiostreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
