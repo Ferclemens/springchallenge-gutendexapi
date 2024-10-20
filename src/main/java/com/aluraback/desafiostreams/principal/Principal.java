dce271efe01e65eb3b58d70ac04a0d4777509876
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-10-20 10:56:09 -0300

    se agrega busqueda de libro por titulo

--- src/main/java/com/aluraback/desafiostreams/principal/Principal.java
+++ src/main/java/com/aluraback/desafiostreams/principal/Principal.java
@@ -6,6 +6,7 @@ import com.aluraback.desafiostreams.services.ConsumoAPI;
 import com.aluraback.desafiostreams.services.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Principal {
@@ -28,7 +29,20 @@ public class Principal {
                 .map(l -> l.titulo().toUpperCase())
                 .forEach(System.out::println);
 
-        
+        //Busque da de libros por nombre
+        System.out.println("Ingrese el nombre del libro que desea buscar: ");
+        var tituloABuscar = busqueda.nextLine();
+        json = peticionApi.obtenerDatos(URL_BASE + "?search=" + tituloABuscar.replace(" ", "+"));
+        var datosBusqueda = convertir.obtenerDatos(json, Datos.class);
+        Optional<DatosLibros> libroBuscado = datosBusqueda.listaDeLibros().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(tituloABuscar.toUpperCase()))
+                .findFirst();
+        if(libroBuscado.isPresent()){
+            System.out.println("Libro encontrado");
+            System.out.println(libroBuscado.get());
+        } else {
+            System.out.println("Libro no encontrado");
+        }
     }
 
 }
